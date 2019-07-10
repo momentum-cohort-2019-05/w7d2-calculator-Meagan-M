ca148a92552b17d804e612c4b0b2cac70bd5392e
Meagan-M <meaganmakinson80@gmail.com>
2019-07-10 09:48:48 -0400

    add more CSS, javascript isn't working. Boo.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const display = document.querySelector('#display')
-let total = 0;
+const zeroButton = document.querySelector('#zero')
 const oneButton = document.querySelector('#one')
 const twoButton = document.querySelector('#two')
 const threeButton = document.querySelector('#three')
@@ -8,7 +8,7 @@ const fiveButton = document.querySelector('#five')
 const sixButton =  document.querySelector('#six')
 const sevenButton = document.querySelector('#seven')
 const eightButton = document.querySelector('#eight')
-
+const nineButton = document.querySelector('#nine')
 
 oneButton.addEventListener('click', function () {
     display.value += oneButton.value
@@ -19,31 +19,39 @@ twoButton.addEventListener('click', function () {
     display.value += twoButton.value
 })
 
+
 threeButton.addEventListener('click', function () {
     display.value += threeButton.value
 })
+
 
 fourButton.addEventListener('click', function () {
     display.value += fourButton.value
 })
 
+
 fiveButton.addEventListener('click', function () {
     display.value += fiveButton.value
 })
+
 
 sixButton.addEventListener('click', function () {
     display.value += sixButton.value
 })
 
+
 sevenButton.addEventListener('click', function () {
     display.value += sevenButton.value
 })
+
 
 eightButton.addEventListener('click', function () {
     display.value += eightButton.value
 })
 
-const nineButton = document.querySelector('#nine')
+
 nineButton.addEventListener('click', function () {
     display.value += nineButton.value
 })
+
+// math operations-- need a function to take in the information entered then evaluate the expression and return the answer
